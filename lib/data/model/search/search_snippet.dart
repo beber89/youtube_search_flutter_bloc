bad79f56ccd3ceac86bc6337d501a99a2f281337
Moudbuom <mahmoudbaibars@gmail.com>
2019-06-15 16:18:01 +0200

    07 - Single Video Model

--- lib/data/model/search/search_snippet.dart
+++ lib/data/model/search/search_snippet.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
-import 'package:youtube_search_flutter_bloc/data/model/search/thumbnails.dart';
+import 'package:youtube_search_flutter_bloc/data/model/common/model_common.dart';
 import 'package:youtube_search_flutter_bloc/data/model/serializer/serializers.dart';
 
 part 'search_snippet.g.dart';
--- lib/data/model/serializer/serializers.dart
+++ lib/data/model/serializer/serializers.dart
@@ -6,7 +6,9 @@ import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
+import 'package:youtube_search_flutter_bloc/data/model/detail/model_detail.dart';
 import 'package:youtube_search_flutter_bloc/data/model/search/model_search.dart';
+import 'package:youtube_search_flutter_bloc/data/model/common/model_common.dart';
 
 part 'serializers.g.dart';
 
@@ -17,6 +19,9 @@ part 'serializers.g.dart';
   SearchSnippet,
   Thumbnails,
   Thumbnail,
+  YoutubeVideoResponse,
+  VideoItem,
+  VideoSnippet,
 ])
 final Serializers serializers =
     (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
